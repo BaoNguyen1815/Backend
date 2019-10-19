3bc72bd2676d2d69d7bfcfaf46921e0bd578b205
BaoNguyen1815 <49022463+BaoNguyen1815@users.noreply.github.com>
2019-10-19 18:24:31 +0700

    change

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ const bodyParser = require('body-parser');
 const ApiRouter = require("./router/api");
 const app = express();
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     next(); // cho chuong trinh tiep tuc di
 });
 
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://curnonwatch.herokuapp.com'], credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/curnonwatch", (err)=>{
-    if(err) console.log(err);
+mongoose.connect("mongodb://localhost/curnonwatch", (err) => {
+    if (err) console.log(err);
     else console.log("Database connect Successfully");
 });
 
-app.use("/api",ApiRouter);
+app.use("/api", ApiRouter);
 
-app.listen(6969,(err)=>{
-    if(err) console.log(err)
+app.listen(process.env.PORT || 6969, (err) => {
+    if (err) console.log(err)
     else console.log("Server start successfully");
 });
